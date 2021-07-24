bb4bc14256f27f5579d47d7b544d4a9efb61f867
akhilpune <monsterboy67@gmail.com>
2021-07-24 19:14:41 +0530

    commmit

--- dojima-t1/without-sentry/watchman/setup.sh
+++ dojima-t1/without-sentry/watchman/setup.sh
@@ -7,6 +7,6 @@ HEIMDALL_HOME=$HOME/.heimdalld
 heimdalld init
 
 # copy node directories to home directories
-#cp -rf $NODE_DIR/watchman/config/genesis.json $HEIMDALL_HOME/config/
-#cp -rf $NODE_DIR/watchman/config/config.toml $HEIMDALL_HOME/config/
-#cp -rf $NODE_DIR/watchman/config/heimdall-config.toml $HEIMDALL_HOME/config/
+cp -rf $NODE_DIR/watchman/config/genesis.json $HEIMDALL_HOME/config
+#cp -rf $NODE_DIR/watchman/config/config.toml $HEIMDALL_HOME/config
+cp -rf $NODE_DIR/watchman/config/heimdall-config.toml $HEIMDALL_HOME/config
